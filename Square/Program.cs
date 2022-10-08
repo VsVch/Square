589c726e5b3c807d90ac0e9e275d54a02280e13a
Vasil Valchev <96626766+VsVch@users.noreply.github.com>
2022-10-08 20:07:29 +0300

    Add project files.

---
+++ Square/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Square
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = int.Parse(Console.ReadLine());
+            int b = int.Parse(Console.ReadLine());
+            Console.WriteLine(a*b);
+
+            Console.WriteLine("Hello World!");
+
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
